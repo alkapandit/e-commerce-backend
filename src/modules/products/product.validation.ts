82e8636859080fe641a7efe1f88e714aad57e86b
Alka Pandit <alkapandit6101998@gmail.com>
2026-05-14 23:31:28 +0530

    add: update product validation added

--- src/modules/products/product.validation.ts
+++ src/modules/products/product.validation.ts
@@ -58,3 +58,40 @@ export const addProductValidationSchema = z.array(
     createdBy: z.string().min(1, "createdBy is required"),
   }),
 );
+
+export const updateProductValidationSchema = z.object({
+  id: z.string().min(1, "Product ID is required"),
+  name: z.string().min(1, "Product name is required"),
+
+  description: z.string().optional(),
+
+  price: z.number().positive("Price must be > 0"),
+
+  discountPrice: z.number().optional(),
+
+  categoryId: z.number().optional(),
+
+  subCategoryId: z.string().optional(),
+
+  brand: z.string().optional(),
+
+  sku: z.string().optional(),
+
+  stock: z.number().int().min(0, "Stock cannot be negative"),
+
+  images: z.array(z.string().url("Invalid image URL")).optional(),
+
+  isActive: z.boolean().optional(),
+
+  isFeatured: z.boolean().optional(),
+
+  tags: z.array(z.string()).optional(),
+
+  weight: z.number().optional(),
+
+  dimensions: dimensionsValidation.optional(),
+
+  variants: z.array(productVariantValidation).optional(),
+
+  createdBy: z.string().optional(),
+});
