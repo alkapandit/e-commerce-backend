4156720b8bbfd97d07ae28c6c88bf05503c20f10
Alka Pandit <alkapandit6101998@gmail.com>
2026-07-03 23:20:08 +0530

    fix: verify jwt function added to auth and user routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import * as AuthController from "./auth.controller";
 import { validateBody } from "../../common/middlewares/validation.middleware";
 import { registerValidationSchema } from "./auth.service";
+import { verifyJWT } from "../../common/middlewares/auth.middleware";
 
 const router = Router();
 
@@ -11,10 +12,15 @@ router.post(
   AuthController?.register,
 );
 router.post("/login", validateBody, AuthController?.login);
-router.post("/refreshToken", validateBody, AuthController?.refreshToken);
-router.post("/send-email-otp", AuthController.sendEmailOtp);
-router.post("/verify-email-otp", AuthController.verifyEmailOtp);
-router.post("/send-phone-otp", AuthController.sendPhoneOtp);
-router.post("/verify-phone-otp", AuthController.verifyPhoneOtp);
+router.post(
+  "/refreshToken",
+  verifyJWT,
+  validateBody,
+  AuthController?.refreshToken,
+);
+router.post("/send-email-otp", verifyJWT, AuthController.sendEmailOtp);
+router.post("/verify-email-otp", verifyJWT, AuthController.verifyEmailOtp);
+router.post("/send-phone-otp", verifyJWT, AuthController.sendPhoneOtp);
+router.post("/verify-phone-otp", verifyJWT, AuthController.verifyPhoneOtp);
 
 export default router;
--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { validateBody } from "../../common/middlewares/validation.middleware";
 
 import * as UserController from "./user.controller";
+import { verifyJWT } from "../../common/middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/profile", validateBody, UserController.getProfile);
-router.put("/update", validateBody, UserController.updateProfile);
+router.get("/profile", verifyJWT, validateBody, UserController.getProfile);
+router.put("/update", verifyJWT, validateBody, UserController.updateProfile);
 
 export default router;
